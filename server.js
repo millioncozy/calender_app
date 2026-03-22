eb411e8afe18a7a08071bf837a768347590f292e
millioncozy <rblangrblang0909@gmail.com>
2026-03-22 17:59:51 +0900

    fix server route

--- server.js
+++ server.js
@@ -22,3 +22,6 @@ app.get('/schedule', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running'));
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
